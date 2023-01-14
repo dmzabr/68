65c6d93ee2f9fd9d104f04b75b098b8e3f9ec362
dmzabr <119075451+dmzabr@users.noreply.github.com>
2023-01-14 20:02:46 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Akkerman(M, N));
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+
+    else if (n == 0 && m > 0)
+        return Akkerman(m - 1, 1);
+
+    else
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+}
+
+Console.ReadLine();
